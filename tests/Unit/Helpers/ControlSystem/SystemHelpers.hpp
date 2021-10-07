7fa80729e863a3fbea1e61d423751110a50bb274
Kyle Nelli <knelli@caltech.edu>
2021-10-06 17:04:01 -0700

    Add Rotation control system

--- tests/Unit/Helpers/ControlSystem/SystemHelpers.hpp
+++ tests/Unit/Helpers/ControlSystem/SystemHelpers.hpp
@@ -5,9 +5,13 @@
 
 #include "ControlSystem/Component.hpp"
 #include "ControlSystem/ControlErrors/Expansion.hpp"
+#include "ControlSystem/ControlErrors/Rotation.hpp"
+#include "ControlSystem/DataVectorHelpers.hpp"
 #include "ControlSystem/Systems/Expansion.hpp"
+#include "ControlSystem/Systems/Rotation.hpp"
 #include "ControlSystem/Tags.hpp"
 #include "ControlSystem/Tags/MeasurementTimescales.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
 #include "Domain/FunctionsOfTime/Tags.hpp"
 #include "Domain/Tags.hpp"
 #include "Framework/ActionTesting.hpp"
@@ -42,6 +46,37 @@ struct MockExpansionSystem : control_system::Expansion<DerivOrder> {
               control_system::QueueTags::Center<Horizon>, MeasurementQueue,
               control_system::UpdateControlSystem<
                   deriv_order, control_system::ControlErrors::Expansion>>>(
+          runner, 0, measurement_id, center);
+    }
+  };
+};
+
+template <size_t DerivOrder>
+struct MockRotationSystem : control_system::Rotation<DerivOrder> {
+  struct MeasurementQueue : db::SimpleTag {
+    using type = LinkedMessageQueue<
+        double,
+        tmpl::list<control_system::QueueTags::Center<ah::HorizonLabel::AhA>,
+                   control_system::QueueTags::Center<ah::HorizonLabel::AhB>>>;
+  };
+
+  static constexpr size_t deriv_order = DerivOrder;
+
+  struct process_measurement {
+    template <typename Component, ah::HorizonLabel Horizon,
+              typename Metavariables>
+    static void apply(
+        gsl::not_null<ActionTesting::MockRuntimeSystem<Metavariables>*> runner,
+        const std::array<double, 3>& center_array,
+        const LinkedMessageId<double>& measurement_id) {
+      const DataVector center = array_to_datavector(center_array);
+
+      ActionTesting::simple_action<
+          Component,
+          ::Actions::UpdateMessageQueue<
+              control_system::QueueTags::Center<Horizon>, MeasurementQueue,
+              control_system::UpdateControlSystem<
+                  deriv_order, control_system::ControlErrors::Rotation>>>(
           runner, 0, measurement_id, center);
     }
   };
