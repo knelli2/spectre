7c8fd69b15bafcb97fa1e1f0d9da081f0eab7972
Kyle Nelli <knelli@caltech.edu>
2021-10-15 12:14:57 -0700

    Add DataVectorHelpers

---
+++ src/ControlSystem/DataVectorHelpers.hpp
@@ -0,0 +1,12 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"
+
+template <size_t N>
+DataVector array_to_datavector(const std::array<double, N>& arr);
