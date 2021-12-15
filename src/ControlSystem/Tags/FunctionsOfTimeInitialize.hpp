207ec0cc050a76e5cf747019cf9df57885c4e71f
Kyle Nelli <knelli@caltech.edu>
2021-12-14 18:31:06 -0800

    Changes needed for EvolveGhBinaryBlackHole
    also remove AhA and AhB intrp targets. The control systems have their own.

--- src/ControlSystem/Tags/FunctionsOfTimeInitialize.hpp
+++ src/ControlSystem/Tags/FunctionsOfTimeInitialize.hpp
@@ -89,4 +89,146 @@ struct FunctionsOfTimeInitialize : domain::Tags::FunctionsOfTime,
     return functions_of_time;
   }
 };
+
+namespace detail {
+
+CREATE_HAS_STATIC_MEMBER_VARIABLE(override_functions_of_time)
+CREATE_HAS_STATIC_MEMBER_VARIABLE_V(override_functions_of_time)
+
+template <typename Metavariables, bool HasOverrideCubicFunctionsOfTime>
+struct OptionList {
+  using type = tmpl::conditional_t<
+      Metavariables::override_functions_of_time,
+      tmpl::flatten<tmpl::list<
+          domain::OptionTags::DomainCreator<Metavariables::volume_dim>,
+          domain::FunctionsOfTime::OptionTags::FunctionOfTimeFile,
+          domain::FunctionsOfTime::OptionTags::FunctionOfTimeNameMap,
+          ::OptionTags::InitialTime, ::OptionTags::InitialTimeStep,
+          control_system::inputs<tmpl::transform<
+              tmpl::filter<typename Metavariables::component_list,
+                           tt::is_a_lambda<ControlComponent, tmpl::_1>>,
+              tmpl::bind<tmpl::back, tmpl::_1>>>>>,
+      tmpl::list<domain::OptionTags::DomainCreator<Metavariables::volume_dim>>>;
+};
+
+template <typename Metavariables>
+struct OptionList<Metavariables, false> {
+  using type =
+      tmpl::list<domain::OptionTags::DomainCreator<Metavariables::volume_dim>>;
+};
+}  // namespace detail
+
+/// Tag to retreive the FunctionsOfTime from the GlobalCache.
+struct FunctionsOfTime : db::BaseTag {};
+
+/// \brief The FunctionsOfTime initialized from a DomainCreator or
+/// (if `override_functions_of_time` is true in the metavariables) read
+/// from a file.
+///
+/// \details When `override_functions_of_time == true` in the
+/// metavariables, after obtaining the FunctionsOfTime from the DomainCreator,
+/// one or more of those FunctionsOfTime (which must be cubic piecewise
+/// polynomials) is overriden using data read from an HDF5 file via
+/// domain::Tags::read_spec_piecewise_polynomial()
+struct FunctionsOfTimeSpeCAndSpECTREControlSys : domain::Tags::FunctionsOfTime,
+                                                 db::SimpleTag {
+  using type = std::unordered_map<
+      std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>;
+
+  static constexpr bool pass_metavariables = true;
+
+  static std::string name() { return "FunctionsOfTime"; }
+
+  template <typename Metavariables>
+  using option_tags = typename detail::OptionList<
+      Metavariables,
+      ::detail::has_override_functions_of_time_v<Metavariables>>::type;
+
+  template <typename Metavariables, typename... OptionHolders>
+  static type create_from_options(
+      const std::unique_ptr<::DomainCreator<Metavariables::volume_dim>>&
+          domain_creator,
+      const std::optional<std::string>& function_of_time_file,
+      const std::optional<std::map<std::string, std::string>>&
+          function_of_time_name_map,
+      const double initial_time, const double initial_time_step,
+      const OptionHolders&... option_holders) {
+    if (function_of_time_file and function_of_time_name_map) {
+      const auto initial_expiration_times =
+          control_system::initial_expiration_times(
+              initial_time, initial_time_step, option_holders...);
+
+      auto functions_of_time =
+          domain_creator->functions_of_time(initial_expiration_times);
+
+      // Currently, only support order 2 or 3 piecewise polynomials.
+      // This could be generalized later, but the SpEC functions of time
+      // that we will read in with this action will always be 2nd-order or
+      // 3rd-order piecewise polynomials
+      std::unordered_map<std::string,
+                         domain::FunctionsOfTime::PiecewisePolynomial<2>>
+          spec_functions_of_time_second_order{};
+      std::unordered_map<std::string,
+                         domain::FunctionsOfTime::PiecewisePolynomial<3>>
+          spec_functions_of_time_third_order{};
+
+      // Import those functions of time of each supported order
+      domain::FunctionsOfTime::read_spec_piecewise_polynomial(
+          make_not_null(&spec_functions_of_time_second_order),
+          *function_of_time_file, *function_of_time_name_map);
+      domain::FunctionsOfTime::read_spec_piecewise_polynomial(
+          make_not_null(&spec_functions_of_time_third_order),
+          *function_of_time_file, *function_of_time_name_map);
+
+      for (const auto& [spec_name, spectre_name] : *function_of_time_name_map) {
+        (void)spec_name;
+        // The FunctionsOfTime we are mutating must already have
+        // an element with key==spectre_name; this action only
+        // mutates the value associated with that key
+        if (functions_of_time.count(spectre_name) == 0) {
+          ERROR("Trying to import data for key "
+                << spectre_name
+                << " in FunctionsOfTime, but FunctionsOfTime does not "
+                   "contain that key. This might happen if the option "
+                   "FunctionOfTimeNameMap is not specified correctly. Keys "
+                   "contained in FunctionsOfTime: "
+                << keys_of(functions_of_time) << "\n");
+        }
+        auto* piecewise_polynomial_second_order =
+            dynamic_cast<domain::FunctionsOfTime::PiecewisePolynomial<2>*>(
+                functions_of_time[spectre_name].get());
+        auto* piecewise_polynomial_third_order =
+            dynamic_cast<domain::FunctionsOfTime::PiecewisePolynomial<3>*>(
+                functions_of_time[spectre_name].get());
+        if (piecewise_polynomial_second_order == nullptr) {
+          if (piecewise_polynomial_third_order == nullptr) {
+            ERROR(
+                "The function of time with name "
+                << spectre_name
+                << " is not a PiecewisePolynomial<2> or PiecewisePolynomial<3> "
+                   "and so cannot be set using "
+                   "read_spec_piecewise_polynomial\n");
+          } else {
+            *piecewise_polynomial_third_order =
+                spec_functions_of_time_third_order.at(spectre_name);
+          }
+        } else {
+          *piecewise_polynomial_second_order =
+              spec_functions_of_time_second_order.at(spectre_name);
+        }
+      }
+
+      return functions_of_time;
+    } else {
+      return domain_creator->functions_of_time();
+    }
+  }
+
+  template <typename Metavariables>
+  static type create_from_options(
+      const std::unique_ptr<::DomainCreator<Metavariables::volume_dim>>&
+          domain_creator) {
+    return domain_creator->functions_of_time();
+  }
+};
 }  // namespace control_system::Tags
