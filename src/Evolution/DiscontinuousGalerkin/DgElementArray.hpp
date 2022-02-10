cba1ea033ba7d5fc4162ef7f9f3da2ff06a88492
Kyle Nelli <knelli@caltech.edu>
2022-02-09 17:57:04 -0800

    Add Parallel::Tags::AvoidProc0 to evolution DgElementArray
    and update all input files that use this. Default is false.

--- src/Evolution/DiscontinuousGalerkin/DgElementArray.hpp
+++ src/Evolution/DiscontinuousGalerkin/DgElementArray.hpp
@@ -19,6 +19,7 @@
 #include "Parallel/GlobalCache.hpp"
 #include "Parallel/Local.hpp"
 #include "Parallel/ParallelComponentHelpers.hpp"
+#include "Parallel/Tags/ResourceInfo.hpp"
 #include "Utilities/System/ParallelInfo.hpp"
 #include "Utilities/TMPL.hpp"
 #include "Utilities/TypeTraits/CreateHasStaticMemberVariable.hpp"
@@ -55,9 +56,12 @@ struct DgElementArray {
   using array_allocation_tags =
       tmpl::list<domain::Tags::InitialRefinementLevels<volume_dim>>;
 
-  using initialization_tags = Parallel::get_initialization_tags<
-      Parallel::get_initialization_actions_list<phase_dependent_action_list>,
-      array_allocation_tags>;
+  using initialization_tags =
+      tmpl::append<Parallel::get_initialization_tags<
+                       Parallel::get_initialization_actions_list<
+                           phase_dependent_action_list>,
+                       array_allocation_tags>,
+                   tmpl::list<Parallel::Tags::AvoidProc0>>;
 
   static void allocate_array(
       Parallel::CProxy_GlobalCache<Metavariables>& global_cache,
