0e354de3f59f09997f2528673c7ba1766fd3aa81
Kyle Nelli <knelli@caltech.edu>
2023-01-31 14:45:28 -0800

    DROP. ChangeStepSize debug prints

--- src/Time/Actions/ChangeStepSize.hpp
+++ src/Time/Actions/ChangeStepSize.hpp
@@ -18,6 +18,8 @@
 #include "Utilities/TMPL.hpp"
 #include "Utilities/TaggedTuple.hpp"
 
+#include "Parallel/Printf.hpp"
+
 /// \cond
 class TimeDelta;
 class TimeStepId;
@@ -32,6 +34,9 @@ namespace Tags {
 template <typename Tag>
 struct Next;
 }  // namespace Tags
+namespace Cce::Tags {
+struct BondiBeta;
+}
 // IWYU pragma: no_forward_declare db::DataBox
 /// \endcond
 
@@ -49,6 +54,9 @@ bool change_step_size(const gsl::not_null<db::DataBox<DbTags>*> box) {
   const LtsTimeStepper& time_stepper = db::get<Tags::TimeStepper<>>(*box);
   const auto& step_choosers = db::get<Tags::StepChoosers>(*box);
 
+  constexpr bool is_cce =
+      db::tag_is_retrievable_v<Cce::Tags::BondiBeta, db::DataBox<DbTags>>;
+
   const auto& next_time_id = db::get<Tags::Next<Tags::TimeStepId>>(*box);
   using history_tags = ::Tags::get_all_history_tags<DbTags>;
   bool can_change_step_size = true;
@@ -61,7 +69,22 @@ bool change_step_size(const gsl::not_null<db::DataBox<DbTags>*> box) {
     const auto& history = db::get<tag>(*box);
     can_change_step_size =
         time_stepper.can_change_step_size(next_time_id, history);
+    if constexpr (is_cce) {
+      Parallel::printf("On CCE: For tag %s, can change step size: %s\n",
+                       db::tag_name<tag>(),
+                       can_change_step_size ? "yes" : "no");
+    }
   });
+
+  if constexpr (is_cce) {
+    const auto& current_time_id = db::get<Tags::TimeStepId>(*box);
+    Parallel::printf(
+        "On CCE: Current time = %f, next time = %f, Overall can change step "
+        "size: %s\n",
+        current_time_id.substep_time().value(),
+        next_time_id.substep_time().value(),
+        can_change_step_size ? "yes" : "no");
+  }
   if (not can_change_step_size) {
     return true;
   }
@@ -84,6 +107,10 @@ bool change_step_size(const gsl::not_null<db::DataBox<DbTags>*> box) {
   }
   if (not current_step.is_positive()) {
     desired_step = -desired_step;
+  }
+
+  if constexpr (is_cce) {
+    Parallel::printf("Desired step size is %f\n", desired_step);
   }
 
   if (abs(desired_step / current_step.slab().duration().value()) < 1.0e-9) {
