1a2ae2fb66d3dff753e84f8ca844b49c498e89b5
Kyle Nelli <knelli@caltech.edu>
2023-11-06 11:42:22 -0800

    Use wedge transtiion for rectangular BCO domain

--- src/Domain/Creators/BinaryCompactObject.cpp
+++ src/Domain/Creators/BinaryCompactObject.cpp
@@ -306,25 +306,25 @@ BinaryCompactObject::BinaryCompactObject(
           std::move(outer_boundary_condition), context) {
   time_dependent_options_ = std::move(time_dependent_options);
 
-  std::optional<std::pair<double, double>> inner_outer_radii_A{};
-  std::optional<std::pair<double, double>> inner_outer_radii_B{};
+  std::optional<std::array<double, 3>> radii_A{};
+  std::optional<std::array<double, 3>> radii_B{};
 
   if (is_excised_a_) {
-    inner_outer_radii_A =
-        std::make_pair(std::get<Object>(object_A_).inner_radius,
-                       std::get<Object>(object_A_).outer_radius);
+    radii_A = std::array{std::get<Object>(object_A_).inner_radius,
+                         std::get<Object>(object_A_).outer_radius,
+                         sqrt(3.0) * 0.5 * length_inner_cube_};
   }
   if (is_excised_b_) {
-    inner_outer_radii_B =
-        std::make_pair(std::get<Object>(object_B_).inner_radius,
-                       std::get<Object>(object_B_).outer_radius);
+    radii_B = std::array{std::get<Object>(object_B_).inner_radius,
+                         std::get<Object>(object_B_).outer_radius,
+                         sqrt(3.0) * 0.5 * length_inner_cube_};
   }
 
   if (time_dependent_options_.has_value()) {
     time_dependent_options_->build_maps(
         std::array{std::array{x_coord_a_, 0.0, 0.0},
                    std::array{x_coord_b_, 0.0, 0.0}},
-        inner_outer_radii_A, inner_outer_radii_B, outer_radius_);
+        radii_A, radii_B, outer_radius_);
   }
 }
 
@@ -551,6 +551,11 @@ Domain<3> BinaryCompactObject::create_domain() const {
 
   // Inject the hard-coded time-dependence
   if (time_dependent_options_.has_value()) {
+    if (not(is_excised_a_ and is_excised_b_)) {
+      ERROR(
+          "Kyle has not implemented protections for new distorted frame logic "
+          "with filled spheres for objects. For now, just don't try it.");
+    }
     // Default initialize everything to nullptr so that we only need to set the
     // appropriate block maps for the specific frames
     std::vector<std::unique_ptr<
@@ -579,7 +584,7 @@ Domain<3> BinaryCompactObject::create_domain() const {
     // time-dependent map instead.
     grid_to_inertial_block_maps[number_of_blocks_ - 1] =
         time_dependent_options_
-            ->grid_to_inertial_map<domain::ObjectLabel::None>(false);
+            ->grid_to_inertial_map<domain::ObjectLabel::None>(std::nullopt);
 
     // Inside the excision sphere we add the grid to inertial map from the outer
     // shell. This allows the center of the excisions/horizons to be mapped
@@ -602,54 +607,39 @@ Domain<3> BinaryCompactObject::create_domain() const {
     // the Distorted to Inertial frame. If not excising interior A or B, the
     // layer 1 blocks for that object will not have maps to the Distorted frame
     // (nullptr).
-    grid_to_inertial_block_maps[0] =
-        time_dependent_options_->grid_to_inertial_map<domain::ObjectLabel::A>(
-            is_excised_a_);
-    grid_to_distorted_block_maps[0] =
-        time_dependent_options_->grid_to_distorted_map<domain::ObjectLabel::A>(
-            is_excised_a_);
-    distorted_to_inertial_block_maps[0] =
-        time_dependent_options_->distorted_to_inertial_map(is_excised_a_);
 
     const size_t first_block_object_B = use_single_block_a_ ? 1 : 12;
-    grid_to_inertial_block_maps[first_block_object_B] =
-        time_dependent_options_->grid_to_inertial_map<domain::ObjectLabel::B>(
-            is_excised_b_);
-    grid_to_distorted_block_maps[first_block_object_B] =
-        time_dependent_options_->grid_to_distorted_map<domain::ObjectLabel::B>(
-            is_excised_b_);
-    distorted_to_inertial_block_maps[first_block_object_B] =
-        time_dependent_options_->distorted_to_inertial_map(is_excised_b_);
 
     // Fill in the rest of the block maps by cloning the relevant maps
-    for (size_t block = 1; block < number_of_blocks_ - 1; ++block) {
-      if ((not use_single_block_a_) and block < 6) {
-        // We always have a grid to inertial map. We may or may not have maps to
-        // the distorted frame.
+    for (size_t block = 0; block < number_of_blocks_ - 1; ++block) {
+      if ((not use_single_block_a_) and block < first_block_object_B) {
+        const std::optional<size_t> block_for_distorted_frame = block;
         grid_to_inertial_block_maps[block] =
-            grid_to_inertial_block_maps[0]->get_clone();
-        if (grid_to_distorted_block_maps[0] != nullptr) {
-          grid_to_distorted_block_maps[block] =
-              grid_to_distorted_block_maps[0]->get_clone();
-          distorted_to_inertial_block_maps[block] =
-              distorted_to_inertial_block_maps[0]->get_clone();
-        }
-      } else if (block == first_block_object_B) {
-        continue;  // already initialized
-      } else if ((not use_single_block_b_) and block > first_block_object_B and
-                 block < first_block_object_B + 6) {
-        // We always have a grid to inertial map. We may or may not have maps to
-        // the distorted frame.
+            time_dependent_options_
+                ->grid_to_inertial_map<domain::ObjectLabel::A>(
+                    block_for_distorted_frame);
+        grid_to_distorted_block_maps[block] =
+            time_dependent_options_
+                ->grid_to_distorted_map<domain::ObjectLabel::A>(
+                    block_for_distorted_frame);
+        distorted_to_inertial_block_maps[block] =
+            time_dependent_options_->distorted_to_inertial_map(true);
+      } else if ((not use_single_block_b_) and block >= first_block_object_B and
+                 block < first_block_object_B + 12) {
+        const std::optional<size_t> block_for_distorted_frame =
+            block - first_block_object_B;
         grid_to_inertial_block_maps[block] =
-            grid_to_inertial_block_maps[first_block_object_B]->get_clone();
-        if (grid_to_distorted_block_maps[first_block_object_B] != nullptr) {
-          grid_to_distorted_block_maps[block] =
-              grid_to_distorted_block_maps[first_block_object_B]->get_clone();
-          distorted_to_inertial_block_maps[block] =
-              distorted_to_inertial_block_maps[first_block_object_B]
-                  ->get_clone();
-        }
+            time_dependent_options_
+                ->grid_to_inertial_map<domain::ObjectLabel::B>(
+                    block_for_distorted_frame);
+        grid_to_distorted_block_maps[block] =
+            time_dependent_options_
+                ->grid_to_distorted_map<domain::ObjectLabel::B>(
+                    block_for_distorted_frame);
+        distorted_to_inertial_block_maps[block] =
+            time_dependent_options_->distorted_to_inertial_map(true);
       } else {
+        // No distorted frame
         grid_to_inertial_block_maps[block] =
             grid_to_inertial_block_maps[number_of_blocks_ - 1]->get_clone();
       }
