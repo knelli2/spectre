2098301c2def2e16b6f35962dfe171118fb98669
Kyle Nelli <knelli@caltech.edu>
2025-01-22 20:18:39 -0800

    Add InertialCenters control system to BNS

--- src/Evolution/Executables/GrMhd/GhValenciaDivClean/GhValenciaDivCleanBase.hpp
+++ src/Evolution/Executables/GrMhd/GhValenciaDivClean/GhValenciaDivCleanBase.hpp
@@ -12,6 +12,7 @@
 #include "ControlSystem/Measurements/BNSCenterOfMass.hpp"
 #include "ControlSystem/Metafunctions.hpp"
 #include "ControlSystem/Systems/Expansion.hpp"
+#include "ControlSystem/Systems/InertialCenters.hpp"
 #include "ControlSystem/Systems/Rotation.hpp"
 #include "ControlSystem/Systems/Translation.hpp"
 #include "ControlSystem/Trigger.hpp"
@@ -371,7 +372,8 @@ struct GhValenciaDivCleanTemplateBase<
       use_control_systems,
       tmpl::list<control_system::Systems::Rotation<3, measurement>,
                  control_system::Systems::Expansion<2, measurement>,
-                 control_system::Systems::Translation<2, measurement, 2>>,
+                 control_system::Systems::Translation<2, measurement, 2>,
+                 control_system::Systems::InertialCenters<2, measurement>>,
       tmpl::list<>>;
 
   using interpolator_source_vars =
