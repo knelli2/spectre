085099f0bbdf792c5475ef8ce46eb701df250bdf
Kyle Nelli <knelli@caltech.edu>
2026-03-12 17:22:40 -0700

    Possible compute tag bug in DataBox
    Adds a compute tag to CCE ObserveFields for which a tag already exists
    in the DataBox. Found that this fails to compile locally for me with
    clang 16 (as it should), but on mbot with clang 17 it compiles
    sucessfully.

--- src/Evolution/Systems/Cce/Events/ObserveFields.hpp
+++ src/Evolution/Systems/Cce/Events/ObserveFields.hpp
@@ -234,28 +234,24 @@ class ObserveFields : public Event {
                 const std::vector<std::string>& variables_to_observe,
                 const Options::Context& context = {});
 
-  using compute_tags_for_observation_box =
-    tmpl::list<Tags::Psi0Compute, Tags::Psi1Compute, Tags::Psi2Compute,
-               Tags::SwshDerivativeCompute<Tags::BondiJ,
-                                                  Spectral::Swsh::Tags::Eth>,
-               Tags::SwshDerivativeCompute<Tags::BondiW,
-                                                  Spectral::Swsh::Tags::Eth>,
-               Tags::NewmanPenroseAlphaCompute, Tags::NewmanPenroseBetaCompute,
-               Tags::NewmanPenroseGammaCompute,
-               Tags::NewmanPenroseEpsilonCompute,
-               // Tags::NewmanPenroseKappaCompute,
-               // in our choice of tetrad, \kappa=0
-               Tags::NewmanPenroseTauCompute,
-               Tags::NewmanPenroseSigmaCompute, Tags::NewmanPenroseRhoCompute,
-               Tags::NewmanPenrosePiCompute, Tags::NewmanPenroseNuCompute,
-               Tags::NewmanPenroseMuCompute, Tags::NewmanPenroseLambdaCompute,
-               Tags::SwshDerivativeCompute<Tags::NewmanPenrosePi,
-                                                 Spectral::Swsh::Tags::Eth>,
-               Tags::SwshDerivativeCompute<Tags::NewmanPenrosePi,
-                                                 Spectral::Swsh::Tags::Ethbar>,
-               Tags::DyCompute<Tags::NewmanPenrosePi>,
-               Tags::DyCompute<Tags::NewmanPenroseMu>
-      >;
+  using compute_tags_for_observation_box = tmpl::list<
+      Tags::Psi0Compute, Tags::Psi1Compute, Tags::Psi2Compute,
+      Tags::SwshDerivativeCompute<Tags::BondiJ, Spectral::Swsh::Tags::Eth>,
+      Tags::SwshDerivativeCompute<Tags::BondiW, Spectral::Swsh::Tags::Eth>,
+      Tags::NewmanPenroseAlphaCompute, Tags::NewmanPenroseBetaCompute,
+      Tags::NewmanPenroseGammaCompute, Tags::NewmanPenroseEpsilonCompute,
+      // Tags::NewmanPenroseKappaCompute,
+      // in our choice of tetrad, \kappa=0
+      Tags::NewmanPenroseTauCompute, Tags::NewmanPenroseSigmaCompute,
+      Tags::NewmanPenroseRhoCompute, Tags::NewmanPenrosePiCompute,
+      Tags::NewmanPenroseNuCompute, Tags::NewmanPenroseMuCompute,
+      Tags::NewmanPenroseLambdaCompute,
+      Tags::SwshDerivativeCompute<Tags::NewmanPenrosePi,
+                                  Spectral::Swsh::Tags::Eth>,
+      Tags::SwshDerivativeCompute<Tags::NewmanPenrosePi,
+                                  Spectral::Swsh::Tags::Ethbar>,
+      Tags::DyCompute<Tags::NewmanPenrosePi>,
+      Tags::DyCompute<Tags::NewmanPenroseMu>, Tags::DyCompute<Tags::BondiH>>;
 
   using return_tags = tmpl::list<>;
   using argument_tags = tmpl::list<::Tags::ObservationBox>;
